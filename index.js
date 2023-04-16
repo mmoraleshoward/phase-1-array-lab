333f43c228f2e0873448c21daa8fa2b9ed6e9f2f
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-04-16 03:35:52 -0500

    completion of phase 1 array lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    let newCats = [...cats, name];
+    return newCats
+
+}
+
+function prependCat(name){
+    let coolCats = [name, ...cats];
+    return coolCats;
+}
+
+function removeLastCat(){
+    let coolerCats = cats.slice(0,2)
+    return coolerCats
+}
+
+function removeFirstCat(){
+    let coolestCats = cats.slice(1)
+    return coolestCats
+}
